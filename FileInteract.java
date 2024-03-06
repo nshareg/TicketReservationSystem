0eb646546d2e492f09ba85a7abea79066ca7cbe7
Areg <areg_nshanyan@edu.aua.am>
2024-03-06 22:10:52 +0400

    improved functionality

--- FileInteract.java
+++ FileInteract.java
@@ -77,12 +77,15 @@ public class FileInteract {
             e.printStackTrace();
         }
     }
-    public static String getRow(String key, String filename){//method which returns the whole row by search key
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {// reads the file
+    public static String getRow(int key, String filename){//method which returns the whole row by search key
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String rows;
-            while ((rows = reader.readLine()) != null) {//goes until last row
-                String[] credentials = rows.split(":");//splits our row for future comparing
-                if(credentials[0].equals(key)) return rows;//if matched returns row
+            int num = 0;
+            while ((rows = reader.readLine()) != null) {
+                if (num == key) {
+                    return rows;
+                }
+                num++;
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -114,6 +117,19 @@ public class FileInteract {
             e.printStackTrace();
         }
         return 0;
+    }
+
+    public static int getRowNum(String filename){//method which returns number of users in db
+        int num = 1;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {//reads the file
+            String rows;
+            while ((rows = reader.readLine()) != null) {//loops until the last row
+                num++;//increments number of users
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return num;
     }
 }
 
