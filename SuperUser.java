ec4d8b20dd2459a8584ebe3b2bdf1f524bf005dc
Areg <areg_nshanyan@edu.aua.am>
2024-03-06 18:47:05 +0400

    set method for username and password in superclass, also get methods for instances

--- SuperUser.java
+++ SuperUser.java
@@ -1,24 +1,23 @@
 public class SuperUser {
     private String username;
-    private String name;
-    private String surname;
+    private String password;
     public SuperUser(){}
-    public void setName(String name){
-        this.name = name;
+    public void setCreds(String username, String password){//one set method for two instances, because of impossibility to have username without password.
+        if(Login.ifUserExists(username)){//if user exists simply set the instances to variables we have, will be used during initialization of objects from the txt database.
+            this.username = username;
+            this.password = password;
+        }
+        else{//if no user found, sets the instances and creates row regarding new user
+            this.username = username;
+            this.password = password;
+            Login.setAccount(username,password);
+        }
     }
-    public void setSurname(String surname){
-        this.surname = surname;
-    }
-    public void setUsername(String username){
-        this.username = username;
-    }
-    public String getName() {
-        return this.name;
-    }
-    public String getSurname() {
-        return this.surname;
+
+    public String getPassword() {
+        return Login.getCred(this.username, 1);//gets second colum of the user row, which is password
     }
     public String getUsername(){
-        return this.username;
+        return Login.getCred(this.username, 0);//same as password, but the first column
     }
 }
