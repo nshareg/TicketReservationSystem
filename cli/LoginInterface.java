625c0464db30e0ae592af2e0f0ba0996bd4fecef
nshareg <56555928+nshareg@users.noreply.github.com>
2024-05-11 16:49:32 +0400

    Update LoginInterface.java

--- cli/LoginInterface.java
+++ cli/LoginInterface.java
@@ -15,15 +15,27 @@ public class LoginInterface {
             System.out.println("Please login");
             String username = sc.next();
             String password = sc.next();
-            if(current.login(username, password)){
-                logedIn = true;
-                current.register("liverpoolvsbarca");
-                current.getActivityDB();
-                current.logOut();
-                current.exit();
+            if(current.login(username, password) && current.getIfAdmin()){
+                System.out.println("1| add activity 2| remove activity 3| for logout");
+                if(sc.nextint() == 1){
+                    System.out.println("Write all infromation with # used as space");
+                    String temp = sc.next();
+                    current.setNewActivity(temp);
+                }else if(sc.nextint() == 2){
+                    int index = 1;
+                    for(String line : current.getActivityDB()){
+                        System.out.println(index + line);                    
+                    }
+                    System.out.println("Which index to remove?");
+                    int removeIndex = sc.nextint();
+                    current.removeActivityByAdmin(current.getActivityDB()[removeIndex - 1]);
+                }else if(sc.nextint() == 3){
+                    current.logout();
+                    current.exit();
+                }
             }
             else{
-                System.out.println("Wrong login info");
+                System.out.println("Wrong login info or invalid access level");
             }
         }
     }
