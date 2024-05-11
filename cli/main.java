8ad2eacaa93b8621356b188896fae7f9081a88f1
nshareg <56555928+nshareg@users.noreply.github.com>
2024-05-11 16:40:14 +0400

    Update main.java

--- cli/main.java
+++ cli/main.java
@@ -4,8 +4,12 @@ import ui.TicketReservationSystemUI;
 
 public class main {
     public static void main(String[] args) {
+        if(args.length() == 0){
         TicketReservationSystemUI ticketReservationSystemUI = new TicketReservationSystemUI();
+        }
+        else if(args[0].equals("console"){
         LoginInterface lg = new LoginInterface();
         lg.execute();
+        }
     }
 }
